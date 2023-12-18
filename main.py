22d8aa0ecf92fea6ac5ea42206831eb65b268b27
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2023-12-18 20:10:20 +0100

    add bibliography form

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 import arrow
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form, Query
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
+from starlette import status
 
 
-app = FastAPI()
+app = FastAPI(max_body_size=1000000)
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/", response_class=HTMLResponse)
-def read_bibliography(request: Request):
+def read_bibliography(request: Request, bibliography: str = Query(None)):
     current_year = arrow.now().year
-    return templates.TemplateResponse("index.html", {"request": request, "current_year": current_year})
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request,
+            "current_year": current_year,
+            "bibliography": bibliography,
+        }
+    )
+
+
+@app.post("/create_bibliography", response_class=HTMLResponse)
+def create_bibliography(
+    bibliography: str = Form(...),
+):
+    return RedirectResponse(
+        url=f"/?bibliography={bibliography}",
+        status_code=status.HTTP_302_FOUND,
+    )

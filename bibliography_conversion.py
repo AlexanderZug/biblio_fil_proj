d1c900efe2d59683aaf1c020d37a1b5272de0043
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2023-12-25 21:58:49 +0100

    add book

--- bibliography_conversion.py
+++ bibliography_conversion.py
@@ -66,7 +66,7 @@ class ArticleConverter(BibliographyConverter):
             result_str = f"{author} \"{title_trans}\" [{title_en}], {journal}, {self.year.group(1)}, № {self.issue.group(1)}, pp. {self.pages.group(1)}. (In Russian)"
 
             return result_str
-        except AttributeError as e:
+        except AttributeError:
             return (
                 "Библиография прописана некорректно. "
                 "Допустимый формат: Фамилия И.О. Название статьи // Название журнала. Год. №. С. 1–10. "
@@ -75,14 +75,64 @@ class ArticleConverter(BibliographyConverter):
 
 
 class BookConverter(BibliographyConverter):
+    AUTHOR_REGEX = re.compile(r'^([^\.]+\.[^\.]+)\.')
+    TITLE_REGEX = re.compile(r'\.\s*([\w\s\.,:–!?;\"\'«»„“”‘’-]+)\s*[^:]*:')
+    PUBLISH_HOUSE_REGEX = re.compile(r':\s*([^0-9]+)\d{4}')
+    CITY = re.compile(r'\.\s*([^:]+)\s*:')
+    YEAR_REGEX = re.compile(r'\b(\d{4})\b')
+    PAGES_REGEX = re.compile(r'(\d+)\s*[сc]\.')
+
+
     def __init__(self, book: str):
-        self.book = book
+        self.author = self.AUTHOR_REGEX.search(book)
+        self.title = self.TITLE_REGEX.search(book)
+        self.publish_house = self.PUBLISH_HOUSE_REGEX.search(book)
+        self.city = self.CITY.search(book)
+        self.year = self.YEAR_REGEX.search(book)
+        self.pages = self.PAGES_REGEX.search(book)
 
     def get_translation(self) -> dict[str, str]:
-        pass
+        translator = Translator()
+
+        title_translation = translator.translate(" ".join(self.title.group(1).split()[1:-1]), dest="en").text
+        city = self.city.group(1).split()[-1]
+        cities = {"М.": "Moscow", "СПб.": "Saint Petersburg", "Л.": "Leningrad"}
+
+        if city in cities:
+            city_translation = cities[city]
+        else:
+            city_translation = translator.translate(city, dest="en").text
+
+        return {
+            "english_title": title_translation,
+            "english_city": city_translation,
+        }
 
     def get_transliteration(self) -> dict[str, str]:
-        pass
+        author_translit = translit(self.author.group(1), "ru", reversed=True)
+        title_translit = translit(" ".join(self.title.group(1).split()[1:-1]), "ru", reversed=True)
+        publish_house_translit = translit(self.publish_house.group(1), "ru", reversed=True)
+
+        return {
+            "transliterated_author": author_translit,
+            "transliterated_title": title_translit,
+            "transliterated_publish_house": publish_house_translit,
+        }
 
     def get_bibliography(self) -> str:
-        pass
+        try:
+            author = self.get_transliteration().get("transliterated_author", "")
+            title_en = self.get_translation().get("english_title", "")
+            title_translit = self.get_transliteration().get("transliterated_title", "")
+            city = self.get_translation().get("english_city", "")
+            publish_house = self.get_transliteration().get("transliterated_publish_house", "")
+
+            result_str = f"{author}. {title_translit} [{title_en}]. {city}: {publish_house} {self.year.group(1)}. {self.pages.group(1)} pp. (In Russian)"
+
+            return result_str
+        except AttributeError:
+            return (
+                "Библиография прописана некорректно. "
+                "Допустимый формат: Фамилия И.О. Название книги. Город: Издательство, Год. 100 c. "
+                "В настоящий момент рекомендуется избегать употребления двоеточия - : - в названии монографии"
+            )
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from starlette import status
 
 from bibliography_conversion import ArticleConverter, BookConverter
 
+
 app = FastAPI(max_body_size=1000000)
 templates = Jinja2Templates(directory="templates")
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -39,11 +40,6 @@ async def read_bibliography(
 async def create_article_bibliography(
     bibliography: str = Form(...),
 ):
-    if not bibliography:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Bibliography cannot be empty",
-        )
     result = ArticleConverter(bibliography).get_bibliography()
 
     return RedirectResponse(
@@ -52,15 +48,10 @@ async def create_article_bibliography(
     )
 
 
-@app.get("/create_book", response_class=HTMLResponse)
+@app.post("/create_book", response_class=HTMLResponse)
 async def create_book_bibliography(
     bibliography: str = Form(...),
 ):
-    if not bibliography:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Bibliography cannot be empty",
-        )
     result = BookConverter(bibliography).get_bibliography()
 
     return RedirectResponse(
